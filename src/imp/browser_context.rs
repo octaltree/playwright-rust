4b1b8f87ae773d971ebe83d8ed6ac676c97632c1
octaltree <octaltree@gmail.com>
2021-03-03 16:51:26 +0900

    todo

--- src/imp/browser_context.rs
+++ src/imp/browser_context.rs
@@ -3,10 +3,48 @@ use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
 pub(crate) struct BrowserContext {
-    channel: ChannelOwner
+    channel: ChannelOwner,
+    name: String
+}
+
+impl BrowserContext {
+    pub(crate) fn try_new(channel: ChannelOwner) -> Result<Self, ConnectionError> {
+        let Initializer { name } = serde_json::from_value(channel.initializer.clone())?;
+        Ok(Self { channel, name })
+    }
+
+    // TODO: def set_default_navigation_timeout(self, timeout: float) -> None:
+    // TODO: def set_default_timeout(self, timeout: float) -> None:
+    // TODO: def pages(self) -> List[Page]:
+    // TODO: def browser(self) -> Optional["Browser"]:
+    // TODO: async def new_page(self) -> Page:
+    // TODO: async def cookies(self, urls: Union[str, List[str]] = None) -> List[Cookie]:
+    // TODO: async def add_cookies(self, cookies: List[Cookie]) -> None:
+    // TODO: async def clear_cookies(self) -> None:
+    // TODO: async def grant_permissions(
+    // TODO: async def clear_permissions(self) -> None:
+    // TODO: async def set_geolocation(self, geolocation: Geolocation = None) -> None:
+    // TODO: async def set_extra_http_headers(self, headers: Dict[str, str]) -> None:
+    // TODO: async def set_offline(self, offline: bool) -> None:
+    // TODO: async def add_init_script(
+    // TODO: async def expose_binding(
+    // TODO: async def expose_function(self, name: str, callback: Callable) -> None:
+    // TODO: async def route(self, url: URLMatch, handler: RouteHandler) -> None:
+    // TODO: async def unroute(
+    // TODO: def expect_event(
+    // TODO: async def close(self) -> None:
+    // TODO: async def storage_state(self, path: Union[str, Path] = None) -> StorageState:
+    // TODO: async def wait_for_event(
+    // TODO: def expect_page(
 }
 
 impl RemoteObject for BrowserContext {
     fn channel(&self) -> &ChannelOwner { &self.channel }
     fn channel_mut(&mut self) -> &mut ChannelOwner { &mut self.channel }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Initializer {
+    name: String
 }
