23bbfc8442cc7f7beacac82c6cb6f79124337e29
octaltree <octaltree@gmail.com>
2021-03-05 17:25:25 +0900

    connection

--- src/imp/core/connection.rs
+++ src/imp/core/connection.rs
@@ -1,8 +1,7 @@
-use crate::imp::{core::*, playwright::Playwright, prelude::*};
+use crate::imp::{core::*, prelude::*};
 use std::{
     io,
     path::Path,
-    pin::Pin,
     process::{Child, Command, Stdio},
     sync::TryLockError
 };
@@ -170,36 +169,37 @@ impl Context {
     fn dispatch(&mut self, msg: Response) -> Result<(), ConnectionError> {
         match msg {
             Response::Result(msg) => {
-                // let (place, waker) = self
-                //    .callbacks
-                //    .get(&msg.id)
-                //    .ok_or(ConnectionError::CallbackNotFound)?;
-                // let place = match place.upgrade() {
-                //    Some(p) => p,
-                //    None => return Ok(())
-                //};
-                // let waker = match waker.upgrade() {
-                //    Some(x) => x,
-                //    None => return Ok(())
-                //};
-                // log::trace!("success get rc");
-                // let waker: &Option<Waker> = &waker.lock().unwrap();
-                // let waker = match waker {
-                //    Some(x) => x.clone(),
-                //    None => return Ok(())
-                //};
-                // waker.wake();
+                let WaitPlaces { value, waker } = self
+                    .callbacks
+                    .get(&msg.id)
+                    .ok_or(ConnectionError::CallbackNotFound)?;
+                let place = match value.upgrade() {
+                    Some(p) => p,
+                    None => return Ok(())
+                };
+                let waker = match waker.upgrade() {
+                    Some(x) => x,
+                    None => return Ok(())
+                };
+                *place.lock().unwrap() = Some(Ok(msg.body.map(Arc::new).map_err(Arc::new)));
+                let waker: &Option<Waker> = &waker.lock().unwrap();
+                let waker = match waker {
+                    Some(x) => x.clone(),
+                    None => return Ok(())
+                };
+                waker.wake();
                 return Ok(());
             }
             Response::Initial(msg) => {
                 if Method::is_create(&msg.method) {
                     self.create_remote_object(&msg.guid, msg.params)?;
+                    //(&**parent).push_child(r.clone());
                     return Ok(());
                 }
-                // if Method::is_dispose(&msg.method) {
-                //    self.objects.remove(&msg.guid);
-                //    return Ok(());
-                //}
+                if Method::is_dispose(&msg.method) {
+                    self.objects.remove(&msg.guid);
+                    return Ok(());
+                }
                 // object.channel.Emit(method, c.replaceGuidsWithChannels(msg.Params))
             }
         }
@@ -222,7 +222,6 @@ impl Context {
             .ok_or(ConnectionError::ParentNotFound)?;
         let c = ChannelOwner::new(
             self.ctx.clone(),
-            // self.tx.clone(),
             parent.downgrade(),
             typ.to_owned(),
             guid.to_owned(),
@@ -230,7 +229,6 @@ impl Context {
         );
         let r = RemoteArc::try_new(&typ, &self, c)?;
         self.objects.insert(guid, r);
-        //(&**parent).push_child(r.clone());
         Ok(())
     }
 
@@ -275,108 +273,6 @@ impl Context {
     }
 }
 
-// impl Connection {
-
-//    // pub(crate) fn wait_initial_object(c: Weak<Mutex<Connection>>) -> WaitInitialObject {
-//    //    WaitInitialObject(c)
-//    //}
-
-//    // async fn processOneMessage(&mut self) -> Result<(), ConnectionError> {
-//    //    let task = self.transport.next();
-//    //    let msg = match task.await {
-//    //        None => return Err(ConnectionError::ReceiverClosed),
-//    //        Some(msg) => msg
-//    //    };
-//    //    self.dispatch(msg).await?;
-//    //    Ok(())
-//    //}
-
-//    // pub(crate) async fn receive_initializer_message(&mut self) {
-
-//    // let guid = "Playwright";
-
-//    // if guid in self._objects:
-//    //    return self._objects[guid]
-//    // callback = self._loop.create_future()
-
-//    // def callback_wrapper(result: Any) -> None:
-//    //    callback.set_result(result)
-
-//    // self._waiting_for_object[guid] = callback_wrapper
-//    // return await callback
-//    //}
-
-//    fn dispatch(&mut self, msg: Response) -> Result<(), ConnectionError> {
-//        log::trace!("{:?}", msg);
-//        match msg {
-//            Response::Result(msg) => {
-//                log::trace!("result");
-//                let (place, waker) = self
-//                    .callbacks
-//                    .get(&msg.id)
-//                    .ok_or(ConnectionError::CallbackNotFound)?;
-//                let place = match place.upgrade() {
-//                    Some(p) => p,
-//                    None => return Ok(())
-//                };
-//                let waker = match waker.upgrade() {
-//                    Some(x) => x,
-//                    None => return Ok(())
-//                };
-//                log::trace!("success get rc");
-//                *place.lock().unwrap() = Some(Ok(msg.body.map(Arc::new).map_err(Arc::new)));
-//                let waker: &Option<Waker> = &waker.lock().unwrap();
-//                let waker = match waker {
-//                    Some(x) => x.clone(),
-//                    None => return Ok(())
-//                };
-//                waker.wake();
-//                return Ok(());
-//            }
-//            Response::Initial(msg) => {
-//                if Method::is_create(&msg.method) {
-//                    self.create_remote_object(&msg.guid, msg.params)?;
-//                    return Ok(());
-//                }
-//                if Method::is_dispose(&msg.method) {
-//                    self.objects.remove(&msg.guid);
-//                    return Ok(());
-//                }
-//                // object.channel.Emit(method, c.replaceGuidsWithChannels(msg.Params))
-//            }
-//        }
-//        Ok(())
-//    }
-
-//    fn create_remote_object(
-//        &mut self,
-//        parent: &S<Guid>,
-//        params: Map<String, Value>
-//    ) -> Result<(), ConnectionError> {
-//        let CreateParams {
-//            typ,
-//            guid,
-//            initializer
-//        } = serde_json::from_value(params.into())?;
-//        let parent = self
-//            .objects
-//            .get(parent)
-//            .ok_or(ConnectionError::ParentNotFound)?;
-//        let c = ChannelOwner::new(
-//            self.conn.clone(),
-//            // self.tx.clone(),
-//            parent.downgrade(),
-//            typ.to_owned(),
-//            guid.to_owned(),
-//            initializer
-//        );
-//        let r = RemoteArc::try_new(&typ, &self, c)?;
-//        self.objects.insert(guid, r);
-//        //(&**parent).push_child(r.clone());
-//        Ok(())
-//    }
-//}
-
 #[cfg(test)]
 mod tests {
     use crate::imp::core::*;
@@ -384,6 +280,6 @@ mod tests {
     crate::runtime_test!(start, {
         let driver = Driver::install().unwrap();
         let conn = Connection::try_new(&driver.executable()).unwrap();
-        let _running = Connection::start(&conn);
+        Connection::start(&conn);
     });
 }
