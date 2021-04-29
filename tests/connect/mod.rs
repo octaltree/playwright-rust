ab89f9bd5c4ceaf40183a90e2d4f19ed5f7ce6b3
octaltree <octaltree@users.noreply.github.com>
2021-04-29 12:19:10 +0900

    connect_over_cdp with ws

--- tests/connect/mod.rs
+++ tests/connect/mod.rs
@@ -1,6 +1,6 @@
 use super::{free_local_port, install_browser, playwright_with_driver, Which};
 use playwright::api::{page, Browser, BrowserType};
-use std::net::TcpListener;
+use serde::Deserialize;
 
 pub(super) async fn connect_over_cdp(which: Which) {
     let playwright = playwright_with_driver().await;
@@ -11,7 +11,7 @@ pub(super) async fn connect_over_cdp(which: Which) {
     };
 
     http(&browser_type).await;
-    // ws().await;
+    ws(&browser_type).await;
 }
 
 async fn http(browser_type: &BrowserType) {
@@ -53,4 +53,60 @@ async fn http(browser_type: &BrowserType) {
     cdp1.close().await.unwrap();
     cdp2.close().await.unwrap();
     browser.close().await.unwrap();
+}
+
+async fn ws(browser_type: &BrowserType) {
+    let port = free_local_port().unwrap();
+    let browser = browser_type
+        .launcher()
+        .args(&[format!("--remote-debugging-port={}", port)])
+        .launch()
+        .await
+        .unwrap();
+    let ws_endpoint = fetch_ws_endpoint(browser_type, port).await;
+    {
+        let cdp1 = browser_type
+            .connect_over_cdp_builder(&ws_endpoint)
+            .connect_over_cdp()
+            .await
+            .unwrap();
+        assert_eq!(cdp1.contexts().unwrap().len(), 1);
+        cdp1.close().await.unwrap();
+    }
+    {
+        let cdp2 = browser_type
+            .connect_over_cdp_builder(&ws_endpoint)
+            .connect_over_cdp()
+            .await
+            .unwrap();
+        assert_eq!(cdp2.contexts().unwrap().len(), 1);
+        cdp2.close().await.unwrap();
+    }
+    browser.close().await.unwrap();
+}
+
+async fn fetch_ws_endpoint(browser_type: &BrowserType, port: u16) -> String {
+    let browser = browser_type.launcher().launch().await.unwrap();
+    let browser_context = browser.context_builder().build().await.unwrap();
+    let page = browser_context.new_page().await.unwrap();
+    let url = format!("http://localhost:{}/json/version/", port);
+    let (event, goto) = tokio::join!(
+        page.expect_event(page::EventType::Response),
+        page.goto_builder(&url).goto()
+    );
+    goto.unwrap();
+    let response = match event {
+        Ok(page::Event::Response(res)) => res,
+        _ => unreachable!()
+    };
+    let text = response.text().await.unwrap();
+    #[derive(Debug, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    struct De<'a> {
+        web_socket_debugger_url: &'a str
+    }
+    let De {
+        web_socket_debugger_url
+    } = serde_json::from_str(&text).unwrap();
+    web_socket_debugger_url.into()
 }
--- tests/test.rs
+++ tests/test.rs
@@ -132,6 +132,7 @@ async fn start_test_server(port: u16) {
     });
 }
 
+// XXX: non thread safe
 fn free_local_port() -> Option<u16> {
     let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::LOCALHOST, 0);
     std::net::TcpListener::bind(socket)
