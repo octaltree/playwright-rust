a55cdcf4eedb283892afef238fc51aa1c58e7308
octaltree <octaltree@users.noreply.github.com>
2021-04-30 17:23:03 +0900

    copy_to takes body, it can only be used once

--- src/build.rs
+++ src/build.rs
@@ -26,35 +26,33 @@ fn download(url: &str, dest: &Path) {
 fn download(url: &str, dest: &Path) {
     let cache_dir: &Path = "/tmp/build-playwright-rust".as_ref();
     let cached = cache_dir.join("driver.zip");
-    // if cfg!(debug_assertions) {
-    //    let maybe_metadata = cached.metadata().ok();
-    //    let cache_is_file = || {
-    //        maybe_metadata
-    //            .as_ref()
-    //            .map(fs::Metadata::is_file)
-    //            .unwrap_or_default()
-    //    };
-    //    let cache_size = || {
-    //        maybe_metadata
-    //            .as_ref()
-    //            .map(fs::Metadata::len)
-    //            .unwrap_or_default()
-    //    };
-    //    if cache_is_file() && cache_size() > 10000000 {
-    //        fs::copy(cached, dest).unwrap();
-    //        check_size(dest);
-    //        return;
-    //    }
-    //}
+    if cfg!(debug_assertions) {
+        let maybe_metadata = cached.metadata().ok();
+        let cache_is_file = || {
+            maybe_metadata
+                .as_ref()
+                .map(fs::Metadata::is_file)
+                .unwrap_or_default()
+        };
+        let cache_size = || {
+            maybe_metadata
+                .as_ref()
+                .map(fs::Metadata::len)
+                .unwrap_or_default()
+        };
+        if cache_is_file() && cache_size() > 10000000 {
+            fs::copy(cached, dest).unwrap();
+            check_size(dest);
+            return;
+        }
+    }
     let mut resp = reqwest::blocking::get(url).unwrap();
     let mut dest_file = File::create(dest).unwrap();
-    if cfg!(debug_assertions) {
-        fs::create_dir_all(cache_dir).ok();
-        File::create(cached)
-            .ok()
-            .and_then(|mut cached| resp.copy_to(&mut cached).ok());
-    }
     resp.copy_to(&mut dest_file).unwrap();
+    if cfg!(debug_assertions) {
+        fs::create_dir_all(cache_dir).unwrap();
+        fs::copy(dest, cached).unwrap();
+    }
     check_size(dest);
 }
 
