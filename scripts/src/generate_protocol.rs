38de16dc2cc62df0f5c984903e43ccefcaf5c2c8
octaltree <octaltree@users.noreply.github.com>
2022-08-02 20:44:30 +0900

    todo

--- scripts/src/generate_protocol.rs
+++ scripts/src/generate_protocol.rs
@@ -1,1 +1,14 @@
-fn main() {}
+#[macro_use]
+extern crate serde;
+
+fn main() {
+    let protocol: Protocol = serde_yaml::from_reader(std::io::stdin()).unwrap();
+    let t = "";
+    println!("{}\n// vim: foldnestmax=0 ft=rust", t);
+}
+
+#[derive(Debug, Deserialize)]
+struct Protocol(Vec<Interface>);
+
+#[derive(Debug, Deserialize)]
+struct Interface {}
