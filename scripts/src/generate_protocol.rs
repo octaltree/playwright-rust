5f73be5ffe42fe64d9330d5f3eef72de95ff7434
octaltree <octaltree@users.noreply.github.com>
2022-08-03 12:25:31 +0900

    collect_unnamed

--- scripts/src/generate_protocol.rs
+++ scripts/src/generate_protocol.rs
@@ -1,3 +1,4 @@
+use case::CaseExt;
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote, ToTokens, TokenStreamExt};
 use serde::{Deserialize, Serialize};
@@ -13,11 +14,12 @@ fn main() {
 #[serde(transparent)]
 struct Protocol(HashMap<String, Node>);
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Node {
     Enum(Enum),
     Object(Object),
-    Mixin(Mixin),
+    /// fields embedded in others
+    Mixin(Object),
     Interface(Interface)
 }
 
@@ -41,7 +43,7 @@ impl<'de> Deserialize<'de> for Node {
             "object" => Self::Object(Object {
                 properties: a.properties.unwrap()
             }),
-            "mixin" => Self::Mixin(Mixin {
+            "mixin" => Self::Mixin(Object {
                 properties: a.properties.unwrap()
             }),
             "enum" => Self::Enum(Enum {
@@ -67,12 +69,12 @@ impl<'de> Deserialize<'de> for Node {
     }
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, PartialEq, Clone)]
 struct Enum {
     literals: Vec<String>
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, PartialEq, Clone)]
 struct Interface {
     commands: Option<Commands>,
     events: Option<Events>,
@@ -80,23 +82,18 @@ struct Interface {
     initializer: Option<Properties>
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
-struct Mixin {
-    properties: Properties
-}
-
 /// ex. {"close": null}
 type Commands = HashMap<String, Option<Command>>;
 type Events = HashMap<String, Option<Event>>;
 type Properties = HashMap<String, Type>;
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, PartialEq, Clone)]
 #[serde(transparent)]
 struct Object {
     properties: Properties
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, PartialEq, Clone)]
 struct Command {
     parameters: Option<Properties>,
     returns: Option<Properties>,
@@ -106,25 +103,26 @@ struct Command {
     tracing: Option<Tracing>
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, PartialEq, Clone)]
 #[serde(rename_all = "camelCase")]
 struct Tracing {
     snapshot: bool,
     pause_before_input: Option<bool>
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, PartialEq, Clone)]
 struct Event {
     parameters: Properties
 }
 
-#[derive(Debug, Deserialize, PartialEq, Serialize)]
+#[derive(Debug, Deserialize, PartialEq, Clone, Serialize)]
 #[serde(untagged)]
 enum Type {
     Name(String),
     Items {
         r#type: String,
-        items: Box<Type>
+        #[serde(rename = "items")]
+        item_type: Box<Type>
     },
     Literals {
         r#type: String,
@@ -137,10 +135,134 @@ enum Type {
 }
 
 impl ToTokens for Protocol {
-    fn to_tokens(&self, tokens: &mut TokenStream) { todo!() }
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let mut xs = self.0.iter().collect::<Vec<_>>();
+        xs.sort_by_key(|(_, n)| match n {
+            Node::Enum(_) => 2,
+            Node::Object(_) => 4,
+            Node::Mixin(_) => 3,
+            Node::Interface(_) => 1
+        });
+        tokens.append_all(xs.into_iter().map(|(name, node)| node_tokens(name, node)));
+    }
 }
 
-fn node((name, node): (&str, &Node)) { todo!() }
+fn node_tokens(name: &str, node: &Node) -> TokenStream {
+    match node {
+        Node::Enum(x) => enum_tokens(name, x, true),
+        Node::Object(x) | Node::Mixin(x) => object_tokens(name, x),
+        Node::Interface(_) => quote!()
+    }
+}
+
+fn enum_tokens(name: &str, x: &Enum, camel: bool) -> TokenStream {
+    let variants = x
+        .literals
+        .iter()
+        .map(|label| {
+            let orig = camel.then(|| quote!(#[rename=#label])).unwrap_or_default();
+            let label = format_ident!("{}", label.to_camel());
+            quote! {
+                #orig
+                #label
+            }
+        })
+        .collect::<Vec<_>>();
+    let ignore_warn = (!camel)
+        .then(|| quote!(#[allow(non_camel_case_types)]))
+        .unwrap_or_default();
+    let name = format_ident!("{}", name);
+    quote! {
+        #[derive(Debug, Serialize, Deserialize)]
+        #ignore_warn
+        pub enum #name {
+            #(#variants),*
+        }
+    }
+}
+
+fn object_tokens(name: &str, x: &Object) -> TokenStream {
+    let nodes = collect_unnamed_by_properties(vec![name], &x.properties);
+    quote! {}
+}
+
+fn interface_tokens(name: &str, x: &Interface) -> TokenStream {
+    let Interface {
+        commands,
+        events,
+        extends,
+        initializer
+    } = x;
+    quote! {}
+}
+
+fn collect_unnamed_by_properties<'a>(
+    prefix: Vec<&str>,
+    props: &'a Properties
+) -> HashMap<String, Node> {
+    let mut res = HashMap::new();
+    for (name, t) in props.iter() {
+        res.extend(collect_unnamed(&prefix, name, t));
+    }
+    res
+}
+
+fn collect_unnamed<'a>(prefix: &[&str], name: &str, ty: &'a Type) -> HashMap<String, Node> {
+    let mut res = HashMap::new();
+    match ty {
+        Type::Name(_) => {}
+        Type::Items { r#type, item_type } => {
+            assert!(
+                r#type == "array" || r#type == "array?",
+                "Not match Type::Items"
+            );
+            res.extend(collect_unnamed(prefix, name, &item_type));
+        }
+        Type::Literals { r#type, literals } => {
+            assert!(
+                r#type == "enum" || r#type == "enum?",
+                "Not match Type::Literals"
+            );
+            res.insert(
+                prefix
+                    .iter()
+                    .map(|s| -> &str { s })
+                    .chain(std::iter::once(&name as &str))
+                    .map(|s| s.to_camel())
+                    .collect(),
+                Node::Enum(Enum {
+                    literals: literals.clone()
+                })
+            );
+        }
+        Type::Properties { r#type, properties } => {
+            assert!(
+                r#type == "object" || r#type == "object?",
+                "Not match Type::Properties"
+            );
+            res.insert(
+                prefix
+                    .iter()
+                    .map(|s| -> &str { s })
+                    .chain(std::iter::once(&name as &str))
+                    .map(|s| s.to_camel())
+                    .collect(),
+                Node::Object(Object {
+                    properties: properties.clone()
+                })
+            );
+            res.extend(collect_unnamed_by_properties(
+                {
+                    let mut p = prefix.to_vec();
+                    p.push(name);
+                    p
+                },
+                properties
+            ));
+        }
+    }
+    res
+}
 
 #[cfg(test)]
 mod tests {
@@ -156,7 +278,7 @@ mod tests {
         fn add<'a>(dest: &mut Vec<&'a Type>, t: &'a Type) {
             match t {
                 Type::Name(_) => dest.push(t),
-                Type::Items { items, .. } => add(dest, &items),
+                Type::Items { item_type, .. } => add(dest, &item_type),
                 Type::Literals { .. } => {}
                 Type::Properties { properties, .. } => {
                     for (_, t) in properties.iter() {
@@ -167,7 +289,7 @@ mod tests {
         }
         for (_, node) in protocol.0.iter() {
             match node {
-                Node::Object(Object { properties }) | Node::Mixin(Mixin { properties }) => {
+                Node::Object(Object { properties }) | Node::Mixin(Object { properties }) => {
                     for (_, t) in properties {
                         add(&mut types, t);
                     }
